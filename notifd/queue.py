aa7ac9291af8dcd97a5e9863f7d255f0a16d825e
nidhish <72427739+nidhishgajjar@users.noreply.github.com>
2026-04-17 10:24:34 +0530

    Add flush() to drain queue

--- notifd/queue.py
+++ notifd/queue.py
@@ -4,3 +4,17 @@ _queue = []
 def enqueue(channel, payload):
     _queue.append({"channel": channel, "payload": payload})
     return len(_queue)
+
+
+def flush():
+    """Send everything in the queue, return count."""
+    sent = 0
+    for item in _queue:
+        send(item)
+        _queue.pop(0)
+        sent += 1
+    return sent
+
+
+def send(item):
+    pass  # delivery integration goes here
